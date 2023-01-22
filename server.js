f8dafbd3513f95b2d793608da6466f241babda91
seyhark <thayseyhark@gmail.com>
2023-01-22 22:09:00 +0700

    chore: code refactoring

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get(["/", "/:name"], (req, res) => {
-  greeting = "<h1>Hello From Node!</h1>";
+  greeting = "<h1>Welcome to NodeJs!</h1>";
   name = req.params["name"];
   if (name) {
-    res.send(greeting + "</br>and hello to " + name);
+    res.send(greeting + "</br>Hi, " + name);
   } else {
     res.send(greeting);
   }
 });
 
-app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`))
+app.listen(port, () => console.log(`Server is listening on port ${port}!`))
 
